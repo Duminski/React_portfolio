dae99df8a93772b36c31c76bb07ccd299fbbc94c
Unknown <lauraduminn@gmail.com>
2019-02-14 22:26:16 +0100

    Changes initial

---
+++ src/Background.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Background extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Background</h2>
+        <p>Parcours scolaire</p>
+      </div>
+    );
+  }
+}
+
+export default Background;
---
+++ src/Main.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Background from "./Background";
+import Projects from "./Projects";
+import Contact from "./Contact";
+
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div id="test">
+          <h1>Portfolio</h1>
+          <ul className="header">
+            <li><NavLink to="/">Accueil</NavLink></li>
+            <li><NavLink to="/about">Présentation</NavLink></li>
+            <li><NavLink to="/background">Parcours</NavLink></li>
+            <li><NavLink to="/projects">Projets</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/background" component={Background}/>
+            <Route path="/projects" component={Projects}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default Main;
