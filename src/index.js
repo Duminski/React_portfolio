92fb5040f0457c8c6b40f1b2dc913eebef81cccc
Unknown <lauraduminn@gmail.com>
2019-02-16 12:05:16 +0100

    idx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+<<<<<<< Updated upstream
+=======
+import "./index.css";
+>>>>>>> Stashed changes
 import Main from "./Main";
 
 ReactDOM.render(
